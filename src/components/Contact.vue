9ff54fc7a43d569470c9f5cdb33cb3abe8e8f42c
Christopher Kennedy <chris@onebrightlight.com>
2021-12-20 15:12:46 -0800

    Add Netlfy attribute to form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,5 @@
 <script setup>
-  import InputWrapper from '../partials/FormInputWrapper.vue'
-  import Input from '../partials/InputField.vue'
-  import Label from '../partials/Label.vue'
+  import Form from './Form.vue'
 </script>
 
 <template>
@@ -16,25 +14,7 @@
         </div>
         <div class="flex flex-col mt-8 md:justify-center md:mt-0">
           <div class="space-y-2 2xl:space-y-4 xl:text-lg 2xl:text-xl">
-            <form>
-              <fieldset class="space-y-4">
-                <InputWrapper>
-                  <Input name="Name" nameAttrib="name" id="name" required="true" type="text" placeholder="Dade Murphy" />
-                </InputWrapper>
-                <InputWrapper>
-                  <Input name="Email" nameAttrib="email" id="email" type="email" placeholder="zerocool@stuyvesantHS.edu" />
-                </InputWrapper>
-                <InputWrapper>
-                  <Label name="Message" nameAttrib="message" />
-                  <textarea class="h-48 footer-textarea" id="message" name="message" required placeholder="Hi there, you don’t know me, but I have a business proposition for you that I think you won’t want to pass up.
-
-I don’t play well with others, but perhaps that’s because as far as developers go, I just haven't found one as charming as you yet."></textarea>
-                </InputWrapper>
-              </fieldset>
-              <div class="mt-4 text-right">
-                <button class="px-6 py-3 font-bold tracking-wider uppercase rounded-sm bg-tart-600 hover:bg-tart-700 text-mint-500" type="submit">Submit</button>
-              </div>
-            </form>
+            <Form />
           </div>
         </div>
       </div>
---
+++ src/components/Form.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import InputWrapper from '../partials/FormInputWrapper.vue'
+  import Input from '../partials/InputField.vue'
+  import Label from '../partials/Label.vue'
+</script>
+
+<template>
+  <form
+    name="contact-form"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="franks-field"
+  >
+    <input type="hidden" name="form-name" value="contact-form" />
+    <fieldset class="space-y-4">
+      <InputWrapper>
+        <Input name="Name" nameAttrib="name" id="name" required="true" type="text" placeholder="Dade Murphy" />
+      </InputWrapper>
+      <InputWrapper>
+        <Input name="Email" nameAttrib="email" id="email" type="email" placeholder="zerocool@stuyvesantHS.edu" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label name="Message" nameAttrib="message" />
+        <textarea class="h-48 footer-textarea" id="message" name="message" required placeholder="Hi there, you don’t know me, but I have a business proposition for you that I think you won’t want to pass up.
+
+I don’t play well with others, but perhaps that’s because as far as developers go, I just haven't found one as charming as you yet."></textarea>
+      </InputWrapper>
+    </fieldset>
+    <div class="mt-4 text-right">
+      <button class="px-6 py-3 font-bold tracking-wider uppercase rounded-sm bg-tart-600 hover:bg-tart-700 text-mint-500" type="submit">Submit</button>
+    </div>
+  </form>
+</template>
