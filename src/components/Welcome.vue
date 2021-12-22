27caf580b41a9cc4ebe113808ff6748e237cffcf
Christopher Kennedy <chris@onebrightlight.com>
2021-12-21 22:16:21 -0800

    Move border element to fixed top of page

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative py-12 bg-repeat bg-[length:240px] bg-tart-500 bg-hero-pattern overflow-x-hidden border-t-8 border-t-tart-800">
+  <div class="relative py-12 bg-repeat bg-[length:240px] bg-tart-500 bg-hero-pattern overflow-x-hidden">
     <div class="container relative z-10 mx-auto container-padding pt-36">
       <h1 class="text-4xl font-bold md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-jet-700 font-heading">Hey there, I’m<br><span class="text-mint-500">Christopher.</span></h1>
       <p class="max-w-sm mt-4 text-sm md:mt-8 lg:mt-12 text-mint-500 md:text-base md:max-w-md lg:max-w-xl lg:text-xl lg:leading-snug xl:max-w-2xl 2xl:max-w-3xl">I’m a frontend web developer in Oakland that loves building fun, practical things that help everyday people. I’m passionate about CSS, accessibility, and building a better, more inclusive&nbsp;web.</p>
